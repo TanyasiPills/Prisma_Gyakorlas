0bd1e7863902609acad09009d1d479d1706e37b3
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2024-11-13 14:13:07 +0100

    little commit

---
+++ src/songs/dto/create-song.dto.ts
@@ -0,0 +1,24 @@
+import { IsDefined, IsInt, IsISBN, IsNotEmpty, IsString, Min, Max } from "class-validator";
+export class CreateSongDto {
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    author: string;
+  
+    @IsNotEmpty()
+    @IsInt()
+    lenght: number;
+  
+    @IsNotEmpty()
+    @IsInt()
+    price: number;
+
+    @IsNotEmpty()
+    @IsInt()
+    @Max(5)
+    @Min(1)
+    rating: number;
+}
---
+++ src/songs/songs.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { CreateSongDto } from './dto/create-song.dto';
+import { UpdateSongDto } from './dto/update-song.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class SongsService {
+
+  db: PrismaService;
+
+  constructor(db: PrismaService){
+    this.db = db;
+  }
+
+  create(createSongDto: CreateSongDto) {
+    return this.db.song.create({data: createSongDto});
+  }
+
+  findAll() {
+    return this.db.song.findMany();
+  }
+
+  findOne(id: number) {
+    return this.db.song.findFirst({where: {id}});
+  }
+
+  async update(id: number, updateSongDto: UpdateSongDto) {
+    return await this.db.song.update({where: {id}, data: UpdateSongDto});
+  }
+
+  async remove(id: number) {
+    try{
+      return await this.db.song.delete({where: {id}});
+    }
+    catch{return undefined;}
+  }
+
+  findFree(){
+    return this.db.song.findMany({where: {price: 0}})
+  }
+
+  findTop(count: number){
+    return this.db.song.findMany({orderBy: {rating: 'desc'}, take: count});
+  }
+
+  async findArtists(){
+    const content = await this.db.song.groupBy({by: ['author'], _count: {author: true}, orderBy: {_count: {author: 'desc'}}});
+    return content.map(item => ({artist: item.author,numberOfSongs: item._count.author}));
+  }
+}
